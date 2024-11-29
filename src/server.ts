2060672a887f0e5021fdf24f6480e0d029dc82a0
azmain-siam <siamhossain2700@gmail.com>
2024-11-29 11:23:39 +0600

    basic server initialized

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
-function server() {}
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port ${5000}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 server();
